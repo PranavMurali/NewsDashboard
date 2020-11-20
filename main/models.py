25cd66a9e2f11831cdbba03f25778e0bd90a854c
Pranav Murali <pranavmurali2001@gmail.com>
2020-11-20 18:00:30 +0530

    Inital Base

--- main/models.py
+++ main/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
-# Create your models here.
+#databse table stuff
+class marks(models.Model):
+    title=models.CharField(max_length=50)
+    content=models.TextField()
+    date_modified=models.DateTimeField(auto_now=True)
+    author= models.ForeignKey(User,on_delete=models.CASCADE)
--- main/views.py
+++ main/views.py
@@ -2,8 +2,26 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 
+posts=[
+    {
+        "author":"me",
+        "title":"wose",
+        "content":"wo",
+        "date":"23421"
+    },
+    {
+        "author":"meeses",
+        "title":"wosesse",
+        "content":"wsesefo",
+        "date":"2342sef1"
+    }
+]
 def home(request):
-    return HttpResponse('<h1> This is home<h1>')
+    context={
+        "posts":posts,
+        "Title":"Home"
+    }
+    return render(request,'main/home.html',context)
 
 def about(request):
-    return HttpResponse('<h1> This is About<h1>')
+    return render(request,'main/about.html')
