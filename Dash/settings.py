5e95b43c8305af0e45082619a50dd42e74516b8a
Pranav Murali <pranavmurali2001@gmail.com>
2020-11-24 15:01:41 +0530

    yes

--- Dash/settings.py
+++ Dash/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -119,7 +119,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
-
+STATIC_ROOT= os.path.join(BASE_DIR,'staticfiles')
 STATIC_URL = '/static/'
 
 CRISPY_TEMPLATE_PACK="bootstrap4"
